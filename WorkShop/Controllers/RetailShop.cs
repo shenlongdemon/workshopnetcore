c444dc9bbb18238ae86ae92d6f1fc1d48832f47f
Long <thanhlong.nguyen@nfq.asia>
2020-09-12 10:56:14 +0700

    new

---
+++ WorkShop/Controllers/RetailShop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WorkShop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RetailShop : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "RetailShop"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public RetailShop(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
